bafd99dc8c113f820fa08ed3f1ad508a337992c5
diegoviskh <diegolimadevasconcelos@gmail.com>
2024-04-27 22:03:18 -0300

    responsive layout

--- js/function.js
+++ js/function.js
@@ -2,6 +2,7 @@ function openCourse(course){
     const loading = document.getElementById("loading")
     const cube3x3 = document.getElementById("cube3x3")
     const cube2x2 = document.getElementById("cube2x2")
+    const piramix = document.getElementById("piramix")
 
     loading.classList.add('invisible');
 
@@ -9,10 +10,17 @@ function openCourse(course){
         case 1:
             cube3x3.classList.remove('invisible');
             cube2x2.classList.add("invisible");
+            piramix.classList.add("invisible");
         break;
         case 2:
             cube3x3.classList.add('invisible');
             cube2x2.classList.remove("invisible");
+            piramix.classList.add("invisible");
+        break;
+        case 3:
+            cube3x3.classList.add('invisible');
+            cube2x2.classList.add("invisible");
+            piramix.classList.remove("invisible");
         break;
     }
 }
